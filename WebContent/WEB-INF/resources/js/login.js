b16881093b431344435d9faba1ed6c6e435235de
Fredrik Möller <fredrik@systema.no>
2018-10-23 16:10:33 +0200

    bugfix of lang

--- WebContent/WEB-INF/resources/js/login.js
+++ WebContent/WEB-INF/resources/js/login.js
@@ -1,13 +1,19 @@
   var jq = jQuery.noConflict();
   var counterIndex = 0;
-//  var BLOCKUI_OVERLAY_MESSAGE_DEFAULT = "Please wait...";
+  var BLOCKUI_OVERLAY_MESSAGE_DEFAULT = "Please wait...";
   
   function setBlockUI(element){
 	  let lang = getLanguage();
 	  if (lang == undefined) {
 		lang = 'en-GB';
 	  }
-	  jq.blockUI({ css: { fontSize: '22px' }, message: blockUIMessage[lang]});
+
+	  let UIMessage = blockUIMessage[lang];
+	  if (UIMessage == undefined) {
+		  UIMessage = BLOCKUI_OVERLAY_MESSAGE_DEFAULT;
+	  }
+
+	  jq.blockUI({ css: { fontSize: '22px' }, message: UIMessage});
   }
 
   //https://www.w3schools.com/tags/ref_language_codes.asp - https://www.w3schools.com/tags/ref_country_codes.asp
